ec3b6416cc458782165d593c3b3ae1653fe3894e
Justice Odoyi <justiceodoyi@gmail.com>
2026-02-28 14:00:26 +0100

    first commit

---
+++ hello-world.ts
@@ -0,0 +1,10 @@
+// const test: string = "Hello World";
+// console.log("message:", test);
+
+// console.log(process.env.MY_TEST_ENV_VAR);
+
+import process from 'node:process';
+
+const test: string = "Hello World";
+console.log(process.env.MY_TEST_ENV_VAR);
+
